6f5a617d91f570301ca8b11b316913891fb646e5
Patrick Wu <patrickwu8894@gmail.com>
2025-05-14 17:09:00 -0500

    remove log

--- final_project/ingestor/main.py
+++ final_project/ingestor/main.py
@@ -22,9 +22,7 @@ pattern = re.compile(
 def read_and_insert(ser):
     while True:
         line = ser.readline().decode("utf-8", errors="ignore").strip()
-        print("Raw: ", line)
         match = pattern.match(line)
-        print("Match: ", match)
         if match:
             device_id = match.group(1)
             temperature = float(match.group(2))
